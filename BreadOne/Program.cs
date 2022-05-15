7542c401b172c3d005b86576cebe1b715df25fdc
DESKTOP-R59GM00\bbang <bbangwon.chung@gmail.com>
2022-05-15 23:54:11 +0900

    첫번째 Razor Pages 추가

--- BreadOne/Program.cs
+++ BreadOne/Program.cs
@@ -1,5 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//Add Services to the container
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
+
+//Add Endpoints for RazorPages
+app.MapRazorPages();
 
 app.MapGet("/", () => "Hello World!!!");
 
