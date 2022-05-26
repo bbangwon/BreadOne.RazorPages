bc7ec281f1dfd4756a766ae8c4d9e30abde2ef40
DESKTOP-R59GM00\bbang <bbangwon.chung@gmail.com>
2022-05-27 01:26:40 +0900

    인메모리 데이터 모델

---
+++ BreadOne/Models/Portfolio.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+namespace BreadOne.Models
+{
+    /// <summary>
+    /// 모델 클래스: Model, ViewModel, Dto, Object, Entity, ... 접미사를 쓸 수도 있음
+    /// </summary>
+    public class Portfolio
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }        
+        public string Image { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
