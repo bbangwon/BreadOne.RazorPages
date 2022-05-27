a007d74ef16594cd21ceef10474be7604c2312cc
DESKTOP-R59GM00\bbang <bbangwon.chung@gmail.com>
2022-05-28 00:03:46 +0900

    Portfolio Json

---
+++ BreadOne/Services/PortfolioServiceJsonFile.cs
@@ -0,0 +1,20 @@
+﻿using BreadOne.Models;
+using System.Text.Json;
+
+namespace BreadOne.Services
+{
+    public class PortfolioServiceJsonFile
+    {
+        public IEnumerable<Portfolio>? GetPortfolios()
+        {
+            var jsonFileName = @"C:\study\aspnetcore\RazorPages\BreadOne.RazorPages\BreadOne\wwwroot\Portfolios\portfolio.json";
+
+            using var jsonFileReader = File.OpenText(jsonFileName);
+
+            var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+            var portfolios = JsonSerializer.Deserialize<Portfolio[]>(jsonFileReader.ReadToEnd(), options);
+
+            return portfolios;
+        }
+    }
+}
