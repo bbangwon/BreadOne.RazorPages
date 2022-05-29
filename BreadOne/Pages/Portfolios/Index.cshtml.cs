7039a624a4608e7b45b9b1aa67f90e4c99715ab9
DESKTOP-R59GM00\bbang <bbangwon.chung@gmail.com>
2022-05-29 16:55:40 +0900

    Code Behind 생성자 주입으로 사용

--- BreadOne/Pages/Portfolios/Index.cshtml.cs
+++ BreadOne/Pages/Portfolios/Index.cshtml.cs
@@ -1,12 +1,23 @@
-using Microsoft.AspNetCore.Mvc;
+using BreadOne.Models;
+using BreadOne.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BreadOne.Pages.Portfolios
 {
-    public class IndexModel : PageModel
+	public class IndexModel : PageModel
     {
-        public void OnGet()
+		private readonly PortfolioServiceJsonFile _portfolioServiceJsonFile;
+
+		public IndexModel(PortfolioServiceJsonFile portfolioServiceJsonFile)
+		{
+			this._portfolioServiceJsonFile = portfolioServiceJsonFile;
+		}
+
+		public IEnumerable<Portfolio>? Portfolios { get; private set; }
+
+		public void OnGet()
         {
-        }
+			Portfolios = _portfolioServiceJsonFile.GetPortfolios();
+		}
     }
 }
---
+++ BreadOne/Pages/Portfolios/List.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BreadOne.Pages.Portfolios
+{
+    public class ListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
