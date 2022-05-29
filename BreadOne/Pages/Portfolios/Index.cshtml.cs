4031ee3d735ba809027c0b58632e64cc65c8b1e2
DESKTOP-R59GM00\bbang <bbangwon.chung@gmail.com>
2022-05-29 14:33:23 +0900

    Behind Code

---
+++ BreadOne/Pages/Portfolios/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BreadOne.Pages.Portfolios
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
