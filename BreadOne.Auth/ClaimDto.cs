b5530faa2f546e096e7124fe165be423589525a5
DESKTOP-R59GM00\bbang <bbangwon.chung@gmail.com>
2022-06-10 23:42:31 +0900

    로그아웃 기능 추가

---
+++ BreadOne.Auth/ClaimDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BreadOne.Auth
+{
+    /// <summary>
+    /// Data Transfer Object
+    /// </summary>
+    public class ClaimDto
+    {
+        public string? Type { get; set; }
+        public string? Value { get; set; }
+    }
+}
--- BreadOne.Auth/Program.cs
+++ BreadOne.Auth/Program.cs
@@ -1,6 +1,10 @@
+using BreadOne.Auth;
 using Microsoft.AspNetCore.Authentication;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 //서비스 추가
@@ -13,6 +17,10 @@ app.MapGet("/", async context =>
 {
     string content = "<h1>ASP.NET Core 인증과 권한 초간단 코드</h1>";
     //한글 출력 오류시
+    content += "<a href=\"/Login\">로그인</a><br />";
+    content += "<a href=\"/Info\">정보</a><br />";
+    content += "<a href=\"/InfoJson\">정보(JSON)</a><br />";
+    content += "<a href=\"/Logout\">로그아웃</a><br />";
     context.Response.Headers["Content-Type"] = "text/html; charset=utf-8";
     await context.Response.WriteAsync(content);
 });
@@ -21,7 +29,8 @@ app.MapGet("/Login", async context =>
 {
     var claims = new List<Claim>
     {
-        new Claim(ClaimTypes.Name, "User Name")
+        //new Claim(ClaimTypes.Name, "User Name")
+        new Claim(ClaimTypes.Name, "아이디")
     };
     var claimsIdentity = new ClaimsIdentity(claims, "Cookies");
     var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
@@ -34,21 +43,56 @@ app.MapGet("/Login", async context =>
     await context.Response.WriteAsync("<h3>로그인 완료</h3>");
 });
 
+#region Info
 app.MapGet("/Info", async context =>
 {
-    string result = "";
+string result = "";
 
-    if(context.User.Identity?.IsAuthenticated ?? false)
+if (context.User.Identity?.IsAuthenticated ?? false)
+{
+result += $"<h3>로그인 이름: {context.User.Identity.Name}</h3>";
+}
+else
+{
+result += "<h3>로그인 하지 않았습니다.</h3>";
+}
+
+context.Response.Headers["Content-Type"] = "text/html; charset=utf-8";
+await context.Response.WriteAsync(result, Encoding.Default);
+});
+#endregion
+
+#region InfoJson
+app.MapGet("/InfoJson", async context =>
+{
+    string json = "";
+
+    if (context.User.Identity?.IsAuthenticated ?? false)
     {
-        result += $"<h3>로그인 이름: {context.User.Identity.Name}</h3>";
+        //json += "{ \"type\" : \"Name\", \"value\" : \"User Name\" }";
+        var claims = context.User.Claims.Select(c => new ClaimDto { Type = c.Type, Value = c.Value });
+        json += JsonSerializer.Serialize(
+            claims, 
+            new JsonSerializerOptions { Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping });
     }
     else
     {
-        result += "<h3>로그인 하지 않았습니다.</h3>";
+        json += "{}";
     }
 
-    context.Response.Headers["Content-Type"] = "text/html; charset=utf-8";
-    await context.Response.WriteAsync(result, Encoding.Default);
+    context.Response.Headers["Content-Type"] = "application/json; charset=utf-8";
+    await context.Response.WriteAsync(json);
 });
+#endregion
+
+#region 로그아웃
+app.MapGet("/Logout", async context =>
+{
+await context.SignOutAsync();
+
+context.Response.Headers["Content-Type"] = "text/html; charset=utf-8";
+await context.Response.WriteAsync("<h3>로그아웃 완료</h3>");
+}); 
+#endregion
 
 app.Run();
